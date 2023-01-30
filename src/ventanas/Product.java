28d98cd2e98177e92839bc1c1c88c35137d6f624
Juanes <jespin2876@uta.edu.ec>
2023-01-29 23:55:22 -0500

    Agregadas imagenes e introducidas en código

--- src/ventanas/Product.java
+++ src/ventanas/Product.java
@@ -2,13 +2,12 @@
 package ventanas;
 
 import BDD.CRUDCarrito;
-import BDD.Conexion;
 import Carrito.Carrito;
 import Soporte.Dialogs;
 import java.awt.Image;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import javax.swing.JToggleButton;
 
 public class Product extends javax.swing.JPanel {
 
@@ -23,13 +22,23 @@ public class Product extends javax.swing.JPanel {
         this.imagenProducto.setIcon(new ImageIcon(imagenPro.getImage().
                 getScaledInstance(280,280, Image.SCALE_SMOOTH)));
         this.repaint();
+        this.setImage(this.jtbtnCarrito,"/images/agregarCarrito.png");
+        this.setImage(this.jtbtnFavorito,"/images/favoritont.png");
     }
     
     public int getCodigoProducto(){
         return this.codigoProducto;
     }
     
-    private boolean validatePurchase(){
+    private void setImage(JToggleButton boton, String path){
+        ImageIcon image = new ImageIcon(this.getClass().getResource(path));
+        Icon icon =  new ImageIcon(image.getImage().getScaledInstance(
+                58,49,Image.SCALE_SMOOTH));
+        boton.setIcon(icon);
+        boton.repaint();
+    }
+    
+    private boolean validateLogged(){
         if(PantallaInicial.loggedClient == null){
             Dialogs.informationDialog("Debe Iniciar Sesión para realizar una Compra");
             return false;
@@ -39,13 +48,13 @@ public class Product extends javax.swing.JPanel {
     
     public void productoEnCarrito(){
         if(this.baseDatos.estaEnCarrito(this.codigoProducto)){
-            this.jtbtnCarrito.setText("<html><center>Quitar del Carrito</center></html>");
+            this.setImage(this.jtbtnCarrito,"/images/quitarCarrito.png");
             this.jtbtnCarrito.setSelected(true);
         }
     }
     
     public void productoRetiradoCarrito(){
-        this.jtbtnCarrito.setText("<html><center>Agregar al Carrito</center></html>");
+        this.setImage(this.jtbtnCarrito,"/images/agregarCarrito.png");
         this.jtbtnCarrito.setSelected(false);
     }
 
@@ -58,6 +67,7 @@ public class Product extends javax.swing.JPanel {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jtbtnCarrito = new javax.swing.JToggleButton();
+        jtbtnFavorito = new javax.swing.JToggleButton();
 
         setBackground(new java.awt.Color(252, 247, 247));
         setBorder(javax.swing.BorderFactory.createTitledBorder(""));
@@ -74,10 +84,16 @@ public class Product extends javax.swing.JPanel {
 
         jtbtnCarrito.setBackground(new java.awt.Color(204, 204, 0));
         jtbtnCarrito.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jtbtnCarrito.setText("<html><center>Agregar al Carrito</center></html>");
         jtbtnCarrito.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jtbtnCarritoActionPerformed(evt);
+            }
+        });
+
+        jtbtnFavorito.setBackground(new java.awt.Color(204, 204, 0));
+        jtbtnFavorito.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jtbtnFavoritoActionPerformed(evt);
             }
         });
 
@@ -86,24 +102,24 @@ public class Product extends javax.swing.JPanel {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                .addGap(25, 25, 25)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(25, 25, 25)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                        .addGap(0, 0, Short.MAX_VALUE)))
-                                .addGap(23, 23, 23))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel3)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(jtbtnCarrito, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                        .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(29, 29, 29))
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(imagenProducto, javax.swing.GroupLayout.PREFERRED_SIZE, 334, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jtbtnFavorito, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jtbtnCarrito, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(19, 19, 19))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(imagenProducto, javax.swing.GroupLayout.PREFERRED_SIZE, 334, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(14, 14, 14))
         );
         layout.setVerticalGroup(
@@ -115,23 +131,24 @@ public class Product extends javax.swing.JPanel {
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 16, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
-                    .addComponent(jtbtnCarrito, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(26, Short.MAX_VALUE))
+                    .addComponent(jtbtnCarrito, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jtbtnFavorito, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(26, 26, 26))
         );
     }// </editor-fold>//GEN-END:initComponents
 
     private void jtbtnCarritoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtbtnCarritoActionPerformed
-        if(this.jtbtnCarrito.isSelected()&& this.validatePurchase()){
+        if(this.jtbtnCarrito.isSelected()&& this.validateLogged()){
             this.baseDatos.createProductoCarrito(this.codigoProducto);
             if(Carrito.productosCarrito.isVisible() && Carrito.productosCarrito != null){
                 Carrito.productosCarrito.initPanelProductos();
             }
-            this.jtbtnCarrito.setText("<html><center>Quitar del Carrito</center></html>");
+            this.setImage(this.jtbtnCarrito,"/images/quitarCarrito.png");
             return;
-        } else if(!this.jtbtnCarrito.isSelected() && this.validatePurchase()){
+        } else if(!this.jtbtnCarrito.isSelected() && this.validateLogged()){
            int unidades = this.baseDatos.readCantidadUnidades(this.codigoProducto);
             if(unidades == -1){
                 return;
@@ -140,11 +157,15 @@ public class Product extends javax.swing.JPanel {
             if(Carrito.productosCarrito.isVisible() && Carrito.productosCarrito != null){
                 Carrito.productosCarrito.initPanelProductos();
             }
-            this.jtbtnCarrito.setText("<html><center>Agregar al Carrito</center></html>");
+            this.setImage(this.jtbtnCarrito,"/images/agregarCarrito.png");
             return;
         }
         this.jtbtnCarrito.setSelected(false);
     }//GEN-LAST:event_jtbtnCarritoActionPerformed
+
+    private void jtbtnFavoritoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtbtnFavoritoActionPerformed
+        System.out.println("Pressed");
+    }//GEN-LAST:event_jtbtnFavoritoActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -153,5 +174,6 @@ public class Product extends javax.swing.JPanel {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JToggleButton jtbtnCarrito;
+    private javax.swing.JToggleButton jtbtnFavorito;
     // End of variables declaration//GEN-END:variables
 }
