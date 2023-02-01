17f83bc372a48079da1b5bb2fe7cdffa85e6dea4
Sebastchan8 <jsalan5133@uta.edu.ec>
2023-02-01 17:47:27 -0500

    Apertura monedero usuario

--- src/Clases/Cliente.java
+++ src/Clases/Cliente.java
@@ -7,21 +7,21 @@ public class Cliente extends Usuario{
     
     //HASTA QUE NO SE IMPLEMENTE COMO CAMBIO EL MONEDERO, DEBE ESTAR COMENTADO LO QUE ESTA COMENTADO
 
-    //private float monedero;
+    private float monedero;
     private String tarjetaCredito;
     private ArrayList<Integer> carrito;
     
     public Cliente(String cedula, String nombre, String apellido, String direccion,
-            String telefono, String correo,/*float monedero,*/ String tarjeta){
+            String telefono, String correo,float monedero, String tarjeta){
         super(cedula,nombre,apellido,direccion,telefono,correo);
-        //this.monedero = monedero;
+        this.monedero = monedero;
         this.tarjetaCredito = tarjeta;
         this.carrito = new ArrayList();
     }
 
-    /*public float getMonedero() {
+    public float getMonedero() {
         return this.monedero;
-    }*/
+    }
 
     public String getTarjetaCredito() {
         return this.tarjetaCredito;
@@ -31,9 +31,9 @@ public class Cliente extends Usuario{
         return this.carrito;
     }
 
-    /*public void setMonedero(float monedero) {
+    public void setMonedero(float monedero) {
         this.monedero = monedero;
-    }*/
+    }
 
     public void setTarjetaCredito(String tarjetaCredito) {
         this.tarjetaCredito = tarjetaCredito;
